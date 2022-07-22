1defeb840d021c6fa81b821954dace2c723e14bd
Bernardo Martins <vreamartins@gmail.com>
2022-07-22 04:21:49 +0100

    Update wordle.py

--- wordle.py
+++ wordle.py
@@ -1,6 +1,7 @@
 from dic import list_of_words
 from random import choice
 
+
 word_lenght = 5
 
 list_of_words = [x for x in list_of_words if len(x) == word_lenght]
@@ -18,6 +19,11 @@ while True:
     input_player = str(input(" guess:"))
     l_input_player = list(input_player)
     
+    if input_player == win_word:
+        for x in l_input_player:
+            print(x,end= "")
+        print("\n\033[1;31;40m WIN GG \033[0m\n")
+        break
 
     if len(l_input_player) == 5 and (input_player in list_of_words):
         
@@ -27,18 +33,10 @@ while True:
             if l_input_player[x] == win_word[x]:
                 l_input_player[x] = "\033[1;32;40m"+l_input_player[x]+ '\033[0m'
         
-        for x in range(word_lenght):
-            if list(input_player)[x] != win_word[x] and  list(input_player)[x] in list(win_word) and (list(input_player)[:x+1].count(list(input_player)[x]) <= win_word.count(list(input_player)[x])):
+
+            elif list(input_player)[x] != win_word[x] and  list(input_player)[x] in list(win_word) and (list(input_player)[:x+1].count(list(input_player)[x]) <= win_word.count(list(input_player)[x])):
                 l_input_player[x] = "\033[1;33;40m"+l_input_player[x]+'\033[0m'
 
-        if input_player == win_word:
-            
-            for x in l_input_player:
-                print(x,end= "")
-            print("\n\033[1;31;40m WIN GG \033[0m\n")
-            
-            break
-        
         
         for x in l_input_player:
             print(x,end= "")
